085b4052173b1233f01b572542eea490dd30c8d9
math.vr <math.vr@protonmail.com>
2024-12-02 14:41:52 -0500

    cleanProject

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#gnome-terminal -- sudo docker-compose up --build sqlserver
-#gnome-terminal --window-with-profile=Mathieu -- sudo docker-compose up --build config
-#gnome-terminal  -- sudo docker-compose up --build app
-#sleep 60
-#gnome-terminal -- sudo docker inspect   -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' stocks-app-1
-sudo docker-compose up --build config
-sleep 100
-sudo docker-compose up --build app
